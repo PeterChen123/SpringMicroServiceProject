9e1961bb811dd99a30abb8be514a5ae264baa4d9
Guanyu Chen <chenguanyu9745@outlook.com>
2023-05-26 12:24:58 -0700

    Initial set up limits microservices

---
+++ src/test/java/com/cguanyu/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cguanyu.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
